cc0a73dba1a03764c1d767b94eac97e5edbdf823
Monil <83906560+Monzero@users.noreply.github.com>
2025-09-15 13:46:24 +0530

    Delete schema_config directory

--- schema_config/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Schema configuration dataclasses for project management."""
-
-from .models import FieldDefinition, SchemaDefinition, ProjectConfig
-
-__all__ = [
-    "FieldDefinition",
-    "SchemaDefinition",
-    "ProjectConfig",
-]
